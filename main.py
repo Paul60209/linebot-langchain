d6e7291b19868e192f434c878267fc1818df4d0b
Li Wei, Dai <paul60209@gmail.com>
2023-07-24 15:24:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,14 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents import AgentType
 from langchain.agents import initialize_agent, Tool
 from langchain.schema import HumanMessage
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.sql_database import SQLDatabase
+from langchain.llms.openai import OpenAI
+from langchain.agents import AgentExecutor
+from langchain.agents.agent_types import AgentType
+from langchain.chat_models import ChatOpenAI
+
 
 from stock_price import StockPriceTool
 from stock_peformace import StockPercentageChangeTool, StockGetBestPerformingTool
@@ -43,13 +51,7 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())  # read local .env file
 import mysql.connector
 
-from langchain.agents import create_sql_agent
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
-from langchain.sql_database import SQLDatabase
-from langchain.llms.openai import OpenAI
-from langchain.agents import AgentExecutor
-from langchain.agents.agent_types import AgentType
-from langchain.chat_models import ChatOpenAI
+
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('ChannelSecret', None)
